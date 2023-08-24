79218a46f44e0fee1780774cebda7abea82d64ef
Juli <julianmoring5@gmail.com>
2023-08-24 22:28:36 +0100

    Estado del turno visual

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
+import { useState } from "react"
 import Square from "./Square"
 
-const board = Array(9).fill(null)
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
+
+
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.X)
+  
+
   return (
     <>
       <h1>Tic Tac Toe</h1>
-      <section className="gameBoard">
+      <section 
+      className="gameBoard"
+      >
         {board.map((_, index) => (
         <Square key={index}>
-        
+
         </Square>
       ))}
+      </section>
+      <section className="turn-section">
+        <Square isSelected={turn === TURNS.X} >
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O} >
+          {TURNS.O}
+        </Square>
       </section>
     </>
   )
--- src/Square.jsx
+++ src/Square.jsx
@@ -1,7 +1,11 @@
 // eslint-disable-next-line react/prop-types
-export default function Square ({children}){
+export default function Square ({children, isSelected}){
+  const squareClases = () => {
+    return `square ${isSelected ? 'selected': ''}`
+  }
+  
   return(
-    <div className="square">
+    <div className = {squareClases()}>
       {children}
     </div>
   )
